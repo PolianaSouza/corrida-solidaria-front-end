b5c35eb176ddae09c1c78c46306994fb6058a4df
Poliana Souza <polysantos88@gmail.com>
2023-09-18 16:12:09 -0300

    tada:first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-layout view="hHh lpR fff">
+    <q-header class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+          </q-avatar>
+          Corrida Solidária
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+          </q-avatar>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>Corrida Solidária</h1>
+  </q-page>
+</template>
+
+<script setup></script>

eb2f1af0a3fe758044cba462321a3f39fa377400
gabimottad <gabimottapess@gmail.com>
2023-09-19 21:34:04 -0300

    Criação da pagina principal

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,35 +1,66 @@
 <template>
   <q-layout view="hHh lpR fff">
-    <q-header class="bg-primary text-white">
-      <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
-
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-          </q-avatar>
-          Corrida Solidária
+    <q-header class="bg-primary text-black">
+      <q-toolbar class="transparent-toolbar">
+        <q-toolbar-title align="center">
+          <div class="q-gutter-md q-layout-gutter-x">
+            <q-img
+              src="https://images2.imgbox.com/64/84/wF8M2Jgb_o.png"
+              alt="Ícone"
+              width="55px"
+              height="55px"
+              class="mr-md"
+            />
+            <q-btn
+              dense
+              flat
+              label="Início"
+              @click="navigateTo('inicio')"
+              class="mr-md"
+            />
+            <q-btn
+              dense
+              flat
+              label="Inscrever-se"
+              @click="navigateTo('inscrever')"
+              class="mr-md"
+            />
+            <q-btn
+              dense
+              flat
+              label="Ajudar"
+              @click="navigateTo('ajuda')"
+              class="mr-md"
+            />
+            <q-btn
+              dense
+              flat
+              label="Placar"
+              @click="navigateTo('placar')"
+              class="mr-md"
+            />
+            <q-btn
+              dense
+              flat
+              label="Sobre Nós"
+              @click="navigateTo('sobrenos')"
+              class="mr-md"
+            />
+            <q-btn
+              dense
+              flat
+              class="login-button"
+              label="Login"
+              @click="navigateTo('login')"
+            />
+          </div>
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
-      <!-- drawer content -->
-    </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
-
-    <q-footer class="bg-grey-8 text-white">
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-          </q-avatar>
-        </q-toolbar-title>
-      </q-toolbar>
-    </q-footer>
   </q-layout>
 </template>
 
@@ -41,4 +72,32 @@ const leftDrawerOpen = ref(false);
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value;
 }
+
+function navigateTo(route) {}
 </script>
+
+<style scoped>
+.mr-md {
+  margin-right: 80px;
+}
+.transparent-toolbar {
+  background-color: rgba(255, 255, 255, 0.5);
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+}
+.login-button {
+  background-color: #ffc400;
+  color: #ffffff;
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
+  transition: box-shadow 0.3s;
+  width: 100px;
+}
+
+.login-button:hover {
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+}
+</style>

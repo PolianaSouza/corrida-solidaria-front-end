e4c64b107492a2db3bfeb756e9167978cb4d7c5b
gabimottad <gabimottapess@gmail.com>
2023-09-20 16:08:12 -0300

    Criação das 3 paginas, GABI

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,23 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/InicioPage.vue") }],
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/inscrever",
+    component: () => import("layouts/MainLayoutRegister.vue"),
+    children: [{ path: "", component: () => import("pages/RegisterPage.vue") }],
+  },
+  {
+    path: "/sobre",
+    component: () => import("layouts/MainLayoutSobre.vue"),
+    children: [{ path: "", component: () => import("pages/SobrePage.vue") }],
+  },
+  {
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;

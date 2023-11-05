05ebf5c5153492aa9cba7774580fd794671a15ca
Poliana Souza <polysantos88@gmail.com>
2023-11-05 11:48:55 -0300

    Criação das páginas base

---
+++ src/pages/admin/DashboardPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h5>Dashboard</h5>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/admin/runs/RunsPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h5>Gerenciamento de Corridas</h5>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>

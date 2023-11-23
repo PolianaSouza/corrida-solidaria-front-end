7cd7c89c1ac1818eb8ef5a610419ec13f0589ad5
Higor <higor.b97@hotmail.com>
2023-11-22 21:47:30 -0300

    Adiciona pagina Atletas com view, create, edit e etiqueta

--- src/router/routes.js
+++ src/router/routes.js
@@ -44,6 +44,14 @@ const routes = [
         component: () => import("pages/admin/athletes/AthletesPage.vue"),
       },
       {
+        path: "/admin/atletas/novo-atleta",
+        component: () => import("pages/admin/athletes/FormAthletes.vue"),
+      },
+      {
+        path: "/admin/atletas/tag-atleta",
+        component: () => import("pages/admin/athletes/EtiquetasPage.vue"),
+      },
+      {
         path: "/admin/perfil",
         component: () => import("pages/admin/users/ProfileUser.vue"),
       },

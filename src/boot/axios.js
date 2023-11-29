f5bdb76573889580e025d0f7d942e116c3139be8
Poliana Souza <polysantos88@gmail.com>
2023-11-29 19:45:45 -0300

    inclusão de api

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,7 +1,7 @@
 import { boot } from "quasar/wrappers";
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:3000/" });
+const api = axios.create({ baseURL: "http://179.106.175.178:3000/" });
 
 export default boot(({ app }) => {
   app.config.globalProperties.$axios = axios;
